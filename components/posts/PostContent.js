f9d5ddd3ed12a76e0d6808d94366a6ab12b19fbf
Rodrigo Verruck <rodrigoverruck@gmail.com>
2026-04-21 12:14:41 -0300

    revert: use existing HTML rendering instead of next-mdx-remote

--- components/posts/PostContent.js
+++ components/posts/PostContent.js
@@ -1,20 +1,8 @@
-import { MDXRemote } from 'next-mdx-remote/rsc';
-import StatBand from '../mdx/StatBand';
-import QuoteBand from '../mdx/QuoteBand';
-import Callout from '../mdx/Callout';
-import AffiliateCTA from '../AffiliateCTA';
-
-const mdxComponents = {
-  StatBand,
-  QuoteBand,
-  Callout,
-  AffiliateCTA,
-};
-
 export default function PostContent({ content }) {
   return (
-    <div className="prose prose-lg max-w-[680px] mx-auto text-ink font-sans">
-      <MDXRemote source={content} components={mdxComponents} />
-    </div>
+    <div 
+      className="post-content" 
+      dangerouslySetInnerHTML={{ __html: content }} 
+    />
   );
 }
