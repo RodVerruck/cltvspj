08eff934d22488e06788c97da5e04dbcaf2e2036
Rodrigo Verruck <rodrigoverruck@gmail.com>
2026-04-21 12:11:45 -0300

    feat: connect MDXRemote with prose typography classes

--- components/posts/PostContent.js
+++ components/posts/PostContent.js
@@ -1,7 +1,20 @@
+import { MDXRemote } from 'next-mdx-remote/rsc';
+import StatBand from '../mdx/StatBand';
+import QuoteBand from '../mdx/QuoteBand';
+import Callout from '../mdx/Callout';
+import AffiliateCTA from '../AffiliateCTA';
+
+const mdxComponents = {
+  StatBand,
+  QuoteBand,
+  Callout,
+  AffiliateCTA,
+};
+
 export default function PostContent({ content }) {
   return (
-    <div className="post-content">
-      <div dangerouslySetInnerHTML={{ __html: content }} />
+    <div className="prose prose-lg max-w-[680px] mx-auto text-ink font-sans">
+      <MDXRemote source={content} components={mdxComponents} />
     </div>
   );
 }
