9f2862cf54eb3a5bd2699cbb680e86712262c3cd
Rodrigo Verruck <rodrigoverruck@gmail.com>
2026-04-21 12:17:22 -0300

    fix: replace --- with <hr /> in MDX files for next-mdx-remote

--- components/posts/PostContent.js
+++ components/posts/PostContent.js
@@ -1,8 +1,20 @@
+import { MDXRemote } from 'next-mdx-remote/rsc';
+import StatBand from '../mdx/StatBand';
+import QuoteBand from '../mdx/QuoteBand';
+import Callout from '../mdx/Callout';
+import AffiliateCTA from '../AffiliateCTA';
+
+const components = {
+  StatBand,
+  QuoteBand,
+  Callout,
+  AffiliateCTA,
+};
+
 export default function PostContent({ content }) {
   return (
-    <div 
-      className="post-content" 
-      dangerouslySetInnerHTML={{ __html: content }} 
-    />
+    <div className="prose prose-lg max-w-[680px] mx-auto text-ink font-sans">
+      <MDXRemote source={content} components={components} />
+    </div>
   );
 }
