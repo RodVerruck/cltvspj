65f9eff59952a6eabfa6181ca89738bfa5f6d32e
Rodrigo Verruck <rodrigoverruck@gmail.com>
2026-04-23 08:54:48 -0300

    Update QuoteBand component to use proper HTML quote mark entity (&ldquo;)

--- components/mdx/QuoteBand.js
+++ components/mdx/QuoteBand.js
@@ -2,7 +2,7 @@ export default function QuoteBand({ quote, author }) {
   return (
     <section className="band-breakout band-quote">
       <div className="band-inner band-quote-inner">
-        <span className="quote-mark">"</span>
+        <span className="quote-mark">&ldquo;</span>
         <blockquote className="quote-text">{quote}</blockquote>
         {author && <div className="quote-attribution">{author}</div>}
       </div>
