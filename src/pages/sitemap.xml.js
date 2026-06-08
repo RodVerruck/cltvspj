bd7e8fb140d354629dab74e2da88560d64c6738e
Rodrigo Verruck <rodrigo.verruck@advicehealth.com.br>
2026-06-08 17:44:30 -0300

    feat: enhance sitemap generation with last modified dates for home files and blog posts

--- src/pages/sitemap.xml.js
+++ src/pages/sitemap.xml.js
@@ -1,11 +1,49 @@
+import fs from 'fs';
+import path from 'path';
 import { getAllPosts } from '../lib/posts';
 import { SITE_URL } from '../lib/config';
 
+function toSitemapDate(value) {
+  return new Date(value).toISOString().split('T')[0];
+}
+
+/** Data mais recente entre posts e arquivos da home/calculadora. */
+function getHomeLastMod(posts) {
+  const dates = [];
+
+  if (posts[0]?.date) {
+    dates.push(new Date(posts[0].date));
+  }
+
+  const homeFiles = [
+    'src/pages/index.js',
+    'src/lib/calculator.js',
+    'src/lib/schema/calculator.js',
+  ];
+
+  for (const relativePath of homeFiles) {
+    const fullPath = path.join(process.cwd(), relativePath);
+    if (fs.existsSync(fullPath)) {
+      dates.push(fs.statSync(fullPath).mtime);
+    }
+  }
+
+  if (dates.length === 0) {
+    return toSitemapDate(new Date());
+  }
+
+  const latest = dates.reduce((max, current) => (current > max ? current : max));
+  return toSitemapDate(latest);
+}
+
 function generateSiteMap(posts) {
+  const homeLastMod = getHomeLastMod(posts);
+
   return `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
     <loc>${SITE_URL}</loc>
+    <lastmod>${homeLastMod}</lastmod>
     <changefreq>monthly</changefreq>
     <priority>1.0</priority>
   </url>
@@ -19,7 +57,7 @@ function generateSiteMap(posts) {
       return `
   <url>
     <loc>${SITE_URL}/blog/${slug}</loc>
-    <lastmod>${new Date(date).toISOString().split('T')[0]}</lastmod>
+    <lastmod>${toSitemapDate(date)}</lastmod>
     <changefreq>monthly</changefreq>
     <priority>0.7</priority>
   </url>`;
